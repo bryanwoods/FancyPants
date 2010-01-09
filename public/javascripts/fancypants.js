0db7613272e570246cfa037fb26b21975bdd642a
robertjwhitney <afinitetime@gmail.com>
2010-01-09 14:40:43 -0500

    Adding flash message and fadeOut js

--- public/javascripts/fancypants.js
+++ public/javascripts/fancypants.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $(".fieldWithErrors").next().show();
   $(".fieldWithErrors input[type=hidden]").parent().remove();
+  
+  $('#flashMessage').css('opacity', '.95');
+  setTimeout(function() { $('#flashMessage').fadeOut('slow'); }, 10000);
 });
 //error message goes away on field focus

977d85d1fe8160c943cdd302d8517fac599007a4
robertjwhitney <afinitetime@gmail.com>
2010-01-11 00:57:33 -0500

    adding some comments to js

--- public/javascripts/fancypants.js
+++ public/javascripts/fancypants.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
+  //shows errorMessage
   $(".fieldWithErrors").next().show();
+  /*rails uses hidden fields for things like the checkbox, and still throws errors for any hidden fields 
+  in your form.  This removes the error div so things stay pretty when that happens.*/
   $(".fieldWithErrors input[type=hidden]").parent().remove();
-  
+  //fades out flash message
   $('#flashMessage').css('opacity', '.95');
   setTimeout(function() { $('#flashMessage').fadeOut('slow'); }, 10000);
-  
+  //fades out flash message on "x" button click
   $('#close').click(function () {
     $(this).parent().fadeOut('fast');
   });
